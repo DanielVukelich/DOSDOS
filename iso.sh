a6f3397b123d05d9ab2a484ac2e7b45db4c90920
Daniel Vukelich <Daniel.C.Vukelich@gmail.com>
2014-11-13 14:14:52 -0500

    Initial Commit

---
+++ iso.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+. ./build.sh
+ 
+mkdir -p isodir
+mkdir -p isodir/boot
+mkdir -p isodir/boot/grub
+
+cp grub/efi.img isodir/efi.img
+cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cat > isodir/boot/grub/grub.cfg << EOF
+menuentry "myos" {
+	multiboot /boot/myos.kernel
+}
+EOF
+grub-mkrescue -o myos.iso isodir
