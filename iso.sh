37c29cd7e67129311e88d1aefbf52e9572ba35f1
Daniel Vukelich <Daniel.C.Vukelich@gmail.com>
2015-05-06 15:27:26 -0700

    Tweaked iso script to no longer require useless efi.img

--- iso.sh
+++ iso.sh
@@ -6,11 +6,11 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp grub/efi.img isodir/efi.img
+#cp grub/efi.img isodir/efi.img
 cp sysroot/boot/dosdos.kernel isodir/boot/dosdos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "DOSDOS" {
 	multiboot /boot/dosdos.kernel
 }
 EOF
-grub-mkrescue -o dosdos.iso isodir
+grub-mkrescue -d /usr/lib/grub/i386-pc/ -o dosdos.iso isodir
